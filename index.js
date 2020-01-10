7eebb76be3e7c7e3c16f2898dd74041398f508c3
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-10 19:34:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ function divideBy3(){
   number /= 3
 }
 divideBy3()
- 
+
 console.log(number) // 3.333333333335
- 
+
 add5()
- 
+
 console.log(number) // 8.333333333335
- 
+
 // reset number
 number = 10
- 
+
 add5()
- 
+
 console.log(number) // 15
- 
+
 divideBy3()
- 
+
 console.log(number) /
